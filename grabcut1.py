47503efe1d056536e64b385d73a02e156b615a72
Carlos Garcia <carlosfipq@gmail.com>
2017-04-11 13:33:24 -0600

    retengo la imagen de la webcam

--- grabcut1.py
+++ grabcut1.py
@@ -1,13 +1,44 @@
-# newmask is the mask image I manually labelled
-newmask = cv2.imread('puppets.png',0)
+import cv2
 
-# whereever it is marked white (sure foreground), change mask=1
-# whereever it is marked black (sure background), change mask=0
-mask[newmask == 0] = 0
-mask[newmask == 255] = 1
+# Read the images
+cam = cv2.VideoCapture(0)
+def show_webcam(mirror=False):
+    while True:
+        ret_val, img = cam.read()
+        if mirror:
+            img = cv2.flip(img, 1)
 
-mask, bgdModel, fgdModel = cv2.grabCut(img,mask,None,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_MASK)
+        cv2.imshow('my webcam', img)
+        if cv2.waitKey(1) == 27:
+		          break  # esc to quit
 
-mask = np.where((mask==2)|(mask==0),0,1).astype('uint8')
-img = img*mask[:,:,np.newaxis]
-plt.imshow(img),plt.colorbar(),plt.show()
+    cv2.destroyAllWindows()
+
+
+show_webcam(mirror=True)
+
+foreground = cv2.imread("puppets.png")
+foreground = cv2.resize(foreground, (1040,585))
+background = cv2.imread("ocean.png")
+background = cv2.resize(background, (1040,585))
+alpha = cv2.imread("puppets_alpha.png")
+
+# Convert uint8 to float
+foreground = foreground.astype(float)
+background = background.astype(float)
+
+# Normalize the alpha mask to keep intensity between 0 and 1
+alpha = alpha.astype(float)/255
+
+# Multiply the foreground with the alpha matte
+foreground = cv2.multiply(alpha, foreground)
+
+# Multiply the background with ( 1 - alpha )
+background = cv2.multiply(1.0 - alpha, background)
+
+# Add the masked foreground and background.
+outImage = cv2.add(foreground, background)
+
+# Display image
+cv2.imshow("outImg", outImage/255)
+cv2.waitKey(0)
