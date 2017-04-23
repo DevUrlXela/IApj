dae9fb7f66b3d2cd56b072eeeba2dd678ee5bd38
Carlos Garcia <carlosfipq@gmail.com>
2017-04-23 11:59:35 -0600

    terminado el giro, corto del centro

--- reconocimientoColor/dibujarBanano.py
+++ reconocimientoColor/dibujarBanano.py
@@ -128,3 +128,28 @@ posy = centery - 0.5*distancias[1]
 # NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
 #cv2.imshow("cropped", crop_img)
 #cv2.waitKey(0)
+
+
+imagen = cv2.imread("banana_new.jpg")
+const = 3
+height = np.size(imagen, 0)
+width = np.size(imagen, 1)
+porcentualx = int(height * 0.5)
+porcentualy = int(width * 0.5)
+difh = height - porcentualy
+difw = width - porcentualx
+
+print ("height      ",height)
+print ("width       ",width)
+print ("porcentualx ",porcentualx)
+print ("porcentualy ",porcentualy)
+print ("diferenciah ",difh)
+print ("diferenciaw ",difw)
+
+#crop_img = imagen[porcentualx:porcentualy, difh:difw] # Crop from x, y, w, h -> 100, 200, 300, 400
+crop_img = imagen[porcentualx:width, porcentualy:height] # Crop from x, y, w, h -> 100, 200, 300, 400
+# NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
+print ("pasa")
+cv2.imwrite("cropped.jpeg", crop_img)
+# cv2.imshow("cropped", crop_img)
+# cv2.waitKey(0)
