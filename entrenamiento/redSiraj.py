28ff4215143a99804c5bf16491e834bd3100c1a8
Carlos Garcia <carlosfipq@gmail.com>
2017-05-06 12:45:57 -0600

    terminado entrenamiento red siraj

--- entrenamiento/redSiraj.py
+++ entrenamiento/redSiraj.py
@@ -10,7 +10,7 @@
 
 import numpy as np  # Note: there is a typo on this line in the video
 
-
+import matplotlib.pyplot as pl
 # The following is a function definition of the sigmoid function, which is the type of non-linearity chosen for this neural net. It is not the only type of non-linearity that can be chosen, but is has nice analytical features and is easy to teach with. In practice, large-scale deep learning systems use piecewise-linear functions because they are much less expensive to evaluate.
 #
 # The implementation of this function does double duty. If the deriv=True flag is passed in, the function instead calculates the derivative of the function, which is used in the error backpropogation step.
@@ -34,25 +34,104 @@ X = np.array([[0,0,1],  # Note: there is a typo on this line in the video
             [1,0,1],
             [1,1,1]])
 
-X = np.array([[0.0,0.0,0.0,0.0,0.0,0.0,0.0,92.94],
-    [0.0,0.0,54.61,0.0,0.06,42.39,0.0,1.96],
-    [0.0,0.0,16.1,0.0,0.0,1.43,3.31,79.16],
-    [0.0,0.0,31.33,0.0,0.0,0.0,6.04,62.37]])
+X = np.array([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 92.94],
+            [0.0, 0.0, 54.61, 0.0, 0.06, 42.39, 0.0, 1.96],
+            [0.0, 0.0, 16.1, 0.0, 0.0, 1.43, 3.31, 79.16],
+            [0.0, 0.0, 31.33, 0.0, 0.0, 0.0, 6.04, 62.37],
+            [0.0, 0.0, 28.78, 0.0, 0.0, 0.65, 28.55, 41.94],
+            [0.0, 0.0, 24.24, 1.22, 0.0, 70.18, 0.45, 3.9],
+            [0.0, 0.0, 14.73, 0.0, 0.0, 3.78, 3.1, 78.18],
+            [0.0, 0.0, 0.37, 0.0, 0.0, 84.61, 8.24, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 3.45, 0.0, 55.63],
+            [0.0, 0.0, 0.57, 0.0, 0.0, 84.06, 12.98, 0.0],
+            [1.06, 0.0, 14.37, 0.0, 0.0, 5.47, 38.82, 35.45],
+            [0.0, 0.0, 40.86, 0.02, 0.0, 54.86, 2.88, 0.94],
+            [0.0, 0.0, 17.33, 0.0, 0.0, 51.41, 12.29, 12.27],
+            [0.0, 0.0, 24.1, 0.14, 0.0, 69.73, 0.08, 4.49],
+            [0.0, 0.0, 8.63, 0.0, 0.0, 0.0, 0.0, 91.29],
+            [0.0, 0.0, 42.67, 0.0, 0.0, 0.0, 2.67, 54.65],
+            [0.0, 0.0, 48.57, 0.0, 0.0, 26.12, 0.27, 13.02],
+            [0.0, 0.0, 54.73, 0.0, 0.0, 0.0, 0.0, 45.27],
+            [0.0, 0.0, 8.61, 0.0, 0.0, 67.76, 18.35, 5.29],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 98.47],
+            [0.0, 0.0, 60.02, 0.0, 0.0, 34.41, 4.49, 0.63],
+            [0.0, 0.0, 21.29, 0.0, 0.0, 12.94, 63.12, 2.59],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.18, 0.0, 86.35],
+            [0.0, 0.0, 22.24, 0.0, 0.0, 44.94, 16.71, 13.96],
+            [0.0, 0.0, 0.16, 0.0, 0.0, 68.86, 7.59, 0.0],
+            [0.0, 0.0, 50.51, 0.0, 0.22, 44.12, 0.0, 1.96],
+            [0.0, 0.0, 35.31, 0.43, 0.0, 61.33, 0.92, 1.55],
+            [0.76, 0.0, 0.08, 0.0, 3.0, 26.61, 0.73, 40.78],
+            [0.0, 0.0, 1.86, 0.0, 0.0, 1.88, 0.02, 87.86],
+            [0.0, 0.0, 10.92, 0.0, 0.0, 41.94, 11.47, 35.65],
+            [0.0, 0.0, 4.8, 0.0, 0.0, 0.0, 0.0, 95.02],
+            [0.0, 0.0, 38.18, 0.0, 0.0, 58.69, 2.02, 0.63],
+            [0.55, 0.0, 0.02, 0.0, 3.02, 29.1, 0.65, 37.41],
+            [0.0, 0.0, 10.06, 0.0, 0.0, 0.0, 85.41, 4.53],
+            [0.0, 0.0, 1.78, 0.0, 0.0, 79.86, 17.27, 0.43],
+            [0.0, 0.0, 14.06, 0.0, 0.0, 1.61, 1.73, 81.02],
+            [0.0, 0.0, 22.14, 0.04, 0.0, 72.57, 3.92, 1.29],
+            [0.0, 0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 33.33],
+            [0.0, 0.0, 52.86, 0.0, 0.0, 47.14, 0.0, 0.0],
+            [0.0, 0.0, 86.24, 0.0, 0.0, 0.0, 0.0, 13.76],
+            [0.0, 0.0, 25.29, 0.0, 0.0, 40.49, 20.14, 12.61],
+            [0.0, 0.0, 27.69, 0.0, 0.59, 65.8, 0.0, 0.73],
+            [0.0, 0.0, 31.33, 0.0, 0.0, 5.22, 39.14, 24.16],
+            [0.0, 0.0, 22.8, 0.0, 0.0, 46.94, 0.1, 12.45]])
 
 # The output of the exclusive OR function follows.
 
 # In[26]:
 
 #output data
-y = np.array([[0],
-             [1],
-             [1],
-             [0]])
 
 y = np.array([[0,0,0,0,0,1],
             [1,0,0,0,0,0],
             [0,0,0,0,0,1],
-            [0,0,0,0,1,0]])
+            [0,0,0,0,1,0],
+            [0,0,0,1,0,0],
+            [1,0,0,0,0,0],
+            [0,0,0,0,0,1],
+            [0,0,1,0,0,0],
+            [0,0,0,0,0,1],
+            [0,0,1,0,0,0],
+            [0,0,0,0,0,1],
+            [1,0,0,0,0,0],
+            [0,0,0,1,0,0],
+            [0,1,0,0,0,0],
+            [0,0,0,0,0,1],
+            [0,0,0,0,1,0],
+            [1,0,0,0,0,0],
+            [0,0,0,1,0,0],
+            [0,0,1,0,0,0],
+            [0,0,0,0,0,1],
+            [1,0,0,0,0,0],
+            [0,0,0,1,0,0],
+            [0,0,0,0,0,1],
+            [0,0,0,0,1,0],
+            [0,0,1,0,0,0],
+            [1,0,0,0,0,0],
+            [1,0,0,0,0,0],
+            [0,0,0,0,0,1],
+            [0,0,0,0,0,1],
+            [0,0,0,0,1,0],
+            [0,0,0,0,0,1],
+            [1,0,0,0,0,0],
+            [0,0,0,0,0,1],
+            [0,0,0,1,0,0],
+            [0,0,1,0,0,0],
+            [0,0,0,0,0,1],
+            [1,0,0,0,0,0],
+            [0,0,0,0,0,1],
+            [1,0,0,0,0,0],
+            [0,0,0,0,1,0],
+            [0,0,0,1,0,0],
+            [1,0,0,0,0,0],
+            [0,0,0,1,0,0],
+            [0,0,0,1,0,0]])
+
+
+erroresSum = []
 
 # The seed for the random generator is set so that it will return the same random numbers each time, which is sometimes useful for debugging.
 
@@ -66,8 +145,8 @@ np.random.seed(1)
 # In[28]:
 
 #synapses
-syn0 = 2*np.random.random((8,4)) - 1  # 3x4 matrix of weights ((2 inputs + 1 bias) x 4 nodes in the hidden layer)
-syn1 = 2*np.random.random((4,6)) - 1  # 4x1 matrix of weights. (4 nodes x 1 output) - no bias term in the hidden layer.
+syn0 = 2*np.random.random((8,44)) - 1  # 3x4 matrix of weights ((2 inputs + 1 bias) x 4 nodes in the hidden layer)
+syn1 = 2*np.random.random((44,6)) - 1  # 4x1 matrix of weights. (4 nodes x 1 output) - no bias term in the hidden layer.
 
 
 # This is the main training loop. The output shows the evolution of the error between the model and desired. The error steadily decreases.
@@ -86,9 +165,15 @@ for j in range(60000):
 
     # Back propagation of errors using the chain rule.
     l2_error = y - l2
+
     if(j % 10000) == 0:   # Only print the error every 10000 steps, to save time and limit the amount of output.
 
         print("Error: " + str(np.mean(np.abs(l2_error))))
+
+    if (j % 150) == 0:
+        erroresSum.append(np.mean(np.abs(l2_error)))
+
+
     l2_delta = l2_error*nonlin(l2, deriv=True)
 
     l1_error = l2_delta.dot(syn1.T)
@@ -103,3 +188,17 @@ print("Output after training")
 print(l2)
 print (syn0)
 print (syn1)
+
+#
+# pl.axhline(0, color="black")
+# pl.axvline(0, color="black")
+# pl.plot(np.arange(0., len(erroresSum), 100.),np.array(erroresSum))
+# pl.grid(True)
+# pl.show()
+print (len(erroresSum))
+
+pl.axhline(0, color="black")
+pl.axvline(0, color="black")
+pl.plot(np.arange(0., len(erroresSum), 1.),np.array(erroresSum))
+pl.grid(True)
+pl.show()
