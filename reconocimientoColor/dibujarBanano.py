163de231c97b31dd75b69c8dab6471fbedd577b9
23justo <justojul23@hotmail.com>
2017-05-06 01:21:08 -0600

    funcion ventana agregada a dibujarBanano.py

--- reconocimientoColor/dibujarBanano.py
+++ reconocimientoColor/dibujarBanano.py
@@ -4,14 +4,60 @@ import numpy as np
 #import numpy for scientific calculations
 from matplotlib import pyplot as plt
 #display the image
-from PIL import Image
+from PIL import ImageTk
+from PIL import Image as im
 from scipy import ndimage
+from tkinter import *
+
+
 
 
 green=(0,255,0)
 red=(255,0,0)
 blue=(0,0,255)
 
+def ventana():
+	root = Tk()
+	root.title("Labeler")
+	root.geometry("1050x600")
+
+
+
+	imagen=cv2.imread('ventana/opcion1.jpg')
+	imagen2=cv2.imread('ventana/opcion2.jpg')
+
+	imagen = cv2.resize(imagen,(400, 400))
+	imagen2 = cv2.resize(imagen2,(400, 400))
+
+	cv2.imwrite('ventana/opcion1_resize.jpg',imagen)
+	cv2.imwrite('ventana/opcion2_resize.jpg',imagen2)
+
+	img = ImageTk.PhotoImage(im.open('ventana/opcion1_resize.jpg'))
+	img2 = ImageTk.PhotoImage(im.open('ventana/opcion2_resize.jpg'))
+
+
+	panel = Label(root, image = img)
+	panel2 = Label(root, image = img2)
+	#panel.pack(side = "bottom", fill = "both", expand = "yes")
+	app = Frame(root)
+	app.grid()
+	label = Label(app, text="Elige la opcion que mejor detecto el banano")
+
+	boton1 = Button(app,text="OPCION 1")
+	boton2 = Button(app,text="OPCION 2")
+
+	label.config(font=30)
+	label.grid(row=0, column=200)
+	boton1.grid(row=1, column=150)
+	boton2.grid(row=1, column=200)
+
+	panel.config(height=500)
+	panel.grid(row=40)
+
+	panel2.config(height=500)
+	panel2.grid(row=40,column=80)
+
+	root.mainloop()
 
 
 
@@ -136,3 +182,4 @@ cv2.imwrite('img3.jpg',rotacion)#se crea la segunda ellipse rotada 0 en su angul
 bananaRotada = cv2.imread('img3.jpg')
 result_banana=draw_banana(bananaRotada,np.array([130,0,0]),np.array([170,255,255]))
 bananoLimpio(result_banana,2)
+ventana()
